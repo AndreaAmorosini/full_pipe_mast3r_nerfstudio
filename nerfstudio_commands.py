c15852d9fb40a9ae39b5c0da43ee252e25566835
AndreaAmorosini <andale64@gmail.com>
2025-04-04 12:39:43 +0200

    Added end training logic, and export logic

--- nerfstudio_commands.py
+++ nerfstudio_commands.py
@@ -2,11 +2,25 @@ import subprocess
 import sys
 import time
 import argparse
+import re
 
+config_file_path = ""
 
 def run_command(command):
     print(f"Running command: {' '.join(command)}")
-    process = subprocess.run(command)
+    with subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, bufsize=1) as process:
+        for line in process.stdout:
+            print(line, end="")
+            if "Training Finished" in line:
+                print("Training finished.")
+            match = re.search(r"Config File\s*\|\s*(.+)", line)
+            if match:
+                config_file_path = match.group(1).strip()
+                print(f"Config file path: {config_file_path}")
+                time.sleep(2)  # Optional wait time
+                process.terminate()
+                break
+        process.wait()
     if process.returncode != 0:
         print(f"Command failed: {' '.join(command)}")
         sys.exit(process.returncode)
@@ -38,8 +52,20 @@ def invoke_command(input_path, output_path, colmap_model_path=None, skip_colmap=
     # Step 2: Train using splatfacto
     train_cmd = ["ns-train", "splatfacto", "--data", output_path]
     run_command(train_cmd)
-
-    print("Training complete.")
+    
+    #Step 3: export final .ply
+    export_cmd = [
+        "ns-export",
+        "gaussian-splat",
+        "--load-config",
+        f"{output_path}",
+        "--output-dir",
+        f"{output_path}/exports/splat/",
+    ]
+    run_command(export_cmd)
+    print(".ply exported to", f"{output_path}/exports/splat/")
+    
+    print("Pipe complete.")
 
 
 if __name__ == "__main__":
